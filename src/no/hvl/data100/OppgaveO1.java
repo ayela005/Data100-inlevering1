1910a10ac6c9190cdc8c148b8c79397c7305097b
ayela005 <ayela005@hotmail.com>
2024-09-11 12:48:08 +0200

    Oppgave 01, c)

--- src/no/hvl/data100/OppgaveO1.java
+++ src/no/hvl/data100/OppgaveO1.java
@@ -3,14 +3,27 @@ package no.hvl.data100;
 import static javax.swing.JOptionPane.*;
 import static java.lang.Integer.parseInt; 
 
-public class OppgaveO1 {
+public class OppgaveO1{
 
 	public static void main(String[] args) {
 		
 		for (int i= 0; i< 10; i++) {
+			int poengsummet2; 
 			
+			
+			while(true) {
+		
 				String poengsummet = showInputDialog("Hva er poengsummen til sudent nummer "+ (i+1) + "?");
-				int poengsummet2 = parseInt( poengsummet);
+				 poengsummet2 = parseInt( poengsummet);
+				 
+				 if(poengsummet2>=0 && poengsummet2 <=100) {
+					 break;
+					
+				 } else {
+					 showMessageDialog(null, "Feilmelding: Ugydlig pensum.Poengsummen må være mellom 0 til 100.");
+				 }
+					 
+				 }
 	
 				
 				if (poengsummet2>=90 &&poengsummet2 <=100) {
@@ -26,23 +39,20 @@ public class OppgaveO1 {
 					showMessageDialog(null, "Karakter er  D");
 				} else if (poengsummet2>=40 && poengsummet2<=49) {
 					showMessageDialog(null, " karakter er E ");
-				} else if (poengsummet2>=0 && poengsummet2<=39){
+				} else {
 					showMessageDialog(null, "Karakter er F ");
-				} else { 
-		
-					showMessageDialog(null, "Feilmelding: Ugydlig pengsum.Poengsummen må være mellom 0 til 100.");
-}
 				
- 
-	 
-	 
+	}
 		}
-	 
-	 
- }
-
-
+	}
 }
+
+	 
+ 
+			
+			
+
+
 
 
 
