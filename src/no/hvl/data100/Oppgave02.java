d63a3c8e798a6b92729f84a2eaaea04f8ed3211a
ayela005 <ayela005@hotmail.com>
2024-09-14 14:25:15 +0200

    Oppgave 3 av den  oblikatoriske delen.

--- src/no/hvl/data100/Oppgave02.java
+++ src/no/hvl/data100/Oppgave02.java
@@ -1,10 +1,37 @@
 package no.hvl.data100;
 
+import static javax.swing.JOptionPane.*;
+import static java.lang.Integer.parseInt;
+
 public class Oppgave02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String lønn = showInputDialog(" Hva er din bruttolønn?");
+		int Bruttolønn = parseInt(lønn);
+	
 
+		
+	 if (Bruttolønn<= 280050) {
+		 showMessageDialog( null,"Ingen trinnskatt");	
+	 } else if(Bruttolønn>= 208051 && Bruttolønn<= 292850) {
+		 showMessageDialog( null," Din trinnskatt er på 1,7%");
+	 }
+	 else if (Bruttolønn>= 292851 && Bruttolønn<= 670000) {
+		 showMessageDialog( null, "Din trinnskatt er på 4,0%"); 
+	 }else if (Bruttolønn >=670001 && Bruttolønn<= 937900) {
+		 showMessageDialog(null, "Din trinnskatt er på 13,6%");
+	 }else if (Bruttolønn>=937901 && Bruttolønn<= 1350000) {
+		 showMessageDialog( null,"Din trinnskatt er på 16,6%" ); 
+	 } else if (Bruttolønn>= 1350001) {
+		 showMessageDialog(null, "Din trinnskattt er på 17,6%");
+		 
+	 }
 	}
-
 }
+
+		 
+	
+	 
+
+
--- src/no/hvl/data100/OppgaveO3.java
+++ src/no/hvl/data100/OppgaveO3.java
@@ -1,31 +1,35 @@
 package no.hvl.data100;
 
-import static javax.swing.JOptionPane.*;
-import static java.lang.Integer.parseInt; 
 
+
+import static javax.swing.JOptionPane.*;
+import static java.lang.Integer.parseInt;
 
 public class OppgaveO3 {
 
 	public static void main(String[] args) {
 		
-		String poengsummet = showInputDialog("Hva er poengsummen?" );
-		
-		
-		int karakter;
-		
-		if 
-		int A(90,101);
-		int KarakterB(80,90);
-		int KarakterC(80,80);
-		int karakterD(50,60);
-		int karakterE(40,50);
-		int karakterF(0,40);
-		
-		
-		
-		String poengsummet = showInputDialog("Hva er poengsummen?" )
-		
+		String lønn = showInputDialog(" Hva er din bruttolønn?");
+		int Bruttolønn = parseInt(lønn);
+	
 
+		
+	 if (Bruttolønn<= 280050) {
+		 showMessageDialog( null,"Ingen trinnskatt");	
+	 } else if(Bruttolønn>= 208051 && Bruttolønn<= 292850) {
+		 showMessageDialog( null," Din trinnskatt er på 1,7%");
+	 }
+	 else if (Bruttolønn>= 292851 && Bruttolønn<= 670000) {
+		 showMessageDialog( null, "Din trinnskatt er på 4,0%"); 
+	 }else if (Bruttolønn >=670001 && Bruttolønn<= 937900) {
+		 showMessageDialog(null, "Din trinnskatt er på 13,6%");
+	 }else if (Bruttolønn>=937901 && Bruttolønn<= 1350000) {
+		 showMessageDialog( null,"Din trinnskatt er på 16,6%" ); 
+	 } else if (Bruttolønn>= 1350001) {
+		 showMessageDialog(null, "Din trinnskattt er på 17,6%");
+		 
+	 }
 	}
-
 }
+
+		 
